1a9de9cec7b46bd2b6326c740d0da23a6da7fb84
Frederic <Frederic@roamler.com>
2019-10-22 14:38:34 +0200

    Working Requestor

--- Requestor/Program.cs
+++ Requestor/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading;
@@ -10,59 +12,80 @@ namespace Requestor
     class Program
     {
         static HttpClient http = new HttpClient();
-        static int RequestCount = 0;
+        //static int RequestCount = 0;
         static string path = "hello";
+        static List<Thread> requestThreads = new List<Thread>();
 
         static async Task Main(string[] args)
         {
-            if(args.Length> 0 && !string.IsNullOrEmpty(args[0])) path = args[0];
+            if (args.Length > 0 && !string.IsNullOrEmpty(args[0])) path = args[0];
             Console.WriteLine($"Hammering endpoint {path}");
             Console.WriteLine("Press ESC to stop, up- down- keys to change request count");
-            new Thread(async () =>
-            {
-                Thread.CurrentThread.IsBackground = true;
-                await ParallelBatchRequestLoop();
-            }).Start();
-     
+            http.Timeout = TimeSpan.FromMilliseconds(2500);
+
             while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
             {
                 // do something
                 var k = Console.ReadKey();
                 if (k.Key == ConsoleKey.UpArrow)
                 {
-                    RequestCount++;
-                    Console.WriteLine(RequestCount);
+                    //RequestCount++;
+
+                    var t = new Thread(RequestAndLogLoopAsync);
+                    t.IsBackground = true;
+                    t.Start();
+                    requestThreads.Add(t);
+                    Console.WriteLine();
+                    Console.WriteLine(requestThreads.Count);
                 }
                 if (k.Key == ConsoleKey.DownArrow)
                 {
-                    if (RequestCount > 0) RequestCount--;
-                    Console.WriteLine(RequestCount);
+                    if (requestThreads.Count > 0)
+                    {
+                        //RequestCount--;
+                        var t = requestThreads[0];
+                        t.Interrupt();
+                        requestThreads.Remove(t);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine(requestThreads.Count);
                 }
             }
         }
 
-        static async Task  ParallelBatchRequestLoop()
-        {
-            do{
-                if (RequestCount == 0) continue;
-                var sw = Stopwatch.StartNew();
-                var requestTasks = new List<Task>();
-                for (int i = 0; i < RequestCount; i++)
-                {
-                    requestTasks.Add(RequestAndLogAsync());
-                }
-                await Task.WhenAll(requestTasks);
-                Console.WriteLine($"{RequestCount} requests in {sw.ElapsedMilliseconds}ms");
-                await Task.Delay(10);
-            }while(true);
-        }
+        //static async Task  ParallelBatchRequestLoop()
+        //{
+        //    do{
+        //        if (RequestCount == 0) continue;
+        //        var sw = Stopwatch.StartNew();
+        //        var requestTasks = new List<Task>();
+        //        for (int i = 0; i < RequestCount; i++)
+        //        {
+        //            requestTasks.Add(RequestAndLogLoopAsync());
+        //        }
+        //        await Task.WhenAny(requestTasks);
+        //        Console.WriteLine($"{RequestCount} requests in {sw.ElapsedMilliseconds}ms");
+        //    }while(true);
+        //}
 
-        static async Task RequestAndLogAsync()
+        static void RequestAndLogLoopAsync()
         {
-            var sw = Stopwatch.StartNew();
-            var res = await http.GetAsync($"http://localhost:5000/{path}");
-            var cnt = await res.Content.ReadAsStringAsync();
-            Console.Write($"{sw.ElapsedMilliseconds} ms, ");
+            try
+            {
+                while (true)
+                {
+                    var sw = Stopwatch.StartNew();
+                    var res = http.GetAsync($"http://localhost:5000/{path}").Result;
+                    var cnt = res.Content.ReadAsStringAsync().Result;
+                    Console.Write($"{sw.ElapsedMilliseconds}, ");
+                }
+            }
+            catch (ThreadInterruptedException tie) 
+            { }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+            }
         }
     }
 }
